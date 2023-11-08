94ea3ce695686d5165209d23143720727e580dc7
AchmadDenii <achmaddenii16@gmai.com>
2023-11-08 10:23:15 +0700

    new app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from flask import (
-    Flask,
-    request,
-    render_template,
-    redirect,
-    url_for,
-    jsonify
-)
+from flask import (Flask,request,render_template,redirect,url_for,jsonify)
 from pymongo import MongoClient
 import requests
 from datetime import datetime
